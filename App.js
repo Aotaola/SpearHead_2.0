bf09616a9a7a1df6e51e5bf47e79b4b8a0678a2d
Aotaola <115048502+Aotaola@users.noreply.github.com>
2023-08-21 14:59:22 -0400

    afc_mobile_app first addition

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button, Text, View, Image } from 'react-native';
+import afc_logo from './assets/afc_logo.png';
 
-export default function App() {
+function HomeScreen({navigation}) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      
+      <Button 
+        title="Contact"
+        onPress={() => navigation.navigate('Contact')}
+      />
+      <Button
+        title="Info"
+        onPress={() => navigation.navigate('Info')}  
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function ContactScreen({navigation}) {
+  return (
+    <View>
+      <Text>Contact Screen</Text>
+      <Button
+        title="Info"
+        onPress={() => navigation.navigate('Info')}  
+      />
+      <Button 
+        title = "Home"
+        onPress={() => navigation.navigate('Home')}
+        />
+    </View>
+  );
+}
+
+function InfoScreen({navigation}) {
+  return (
+    <View>
+      <Text>Info Screen</Text>
+      <Button
+        title="Contact"
+        onPress={() => navigation.navigate('Contact')}  
+      />
+      <Button 
+        title = "Home"
+        onPress={() => navigation.navigate('Home')}
+        />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    
+      <NavigationContainer >
+          <Image source = {afc_logo} style={{width: 50, height: 50}} />
+          <Text>American Family Care</Text> 
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Contact" component={ContactScreen} />
+            <Stack.Screen name="Info" component={InfoScreen} />
+          </Stack.Navigator>
+        
+      </NavigationContainer>
+    
+  );
+}
+
+export default App;
+
